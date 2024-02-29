cae6bad4950cd0444c9a956480226300bcae5900
rishab1444 <rishab1444@gmail.com>
2024-02-29 15:46:44 +0530

    Intial Commit

---
+++ ride-sharing-service-loop/src/main/java/com/example/ride/sharing/service/loop/RideSharingServiceLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ride.sharing.service.loop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingServiceLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSharingServiceLoopApplication.class, args);
+	}
+
+}
---
+++ ride-sharing-service-loop/src/test/java/com/example/ride/sharing/service/loop/RideSharingServiceLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ride.sharing.service.loop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSharingServiceLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
